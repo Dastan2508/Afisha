178fcb96e4727fb30dc1e6a856fe7e2eb1924852
Dastan2508 <surancievdastan001@gmail.com>
2024-12-10 20:37:14 +0600

    add

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-
 class Director(models.Model):
     name = models.CharField(max_length=100)
 
@@ -8,20 +6,25 @@ class Director(models.Model):
         return self.name
 
 
+# Модель Movie
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     duration = models.IntegerField()
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='movies')
 
     def __str__(self):
         return self.title
 
+
+STARS = [(i, '* ' * i) for i in range(1, 6)]
+
+
 class Review(models.Model):
     text = models.TextField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name='reviews')
+    stars = models.IntegerField(choices=STARS, default=5)
 
     def __str__(self):
         return self.text
-
-
